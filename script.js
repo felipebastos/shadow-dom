9cafeb723c37d75946d51592d71a32e63bdf4860
Felipe Bastos Nunes <felipebastos@ifce.edu.br>
2024-02-21 14:27:03 -0300

    Entrando dados e saindo informações por meio de eventos

--- script.js
+++ script.js
@@ -2,20 +2,28 @@ class Lista extends HTMLElement {
     constructor() {
         super();
 
+        this.clicados = 0;
 
     }
     connectedCallback() {
         const shadow = this.attachShadow({ mode: "open" });
 
-
-
         const lista = eval(this.getAttribute("texto"));
+        const h2 = document.createElement('h2');
+        h2.className = "clicados";
+        h2.innerText = `Você clicou ${this.clicados} vezes em itens`;
+
+        shadow.appendChild(h2);
 
         const ul = document.createElement('ul');
 
         for (let item of lista) {
             const li = document.createElement('li')
             li.innerText = item;
+            li.onclick = () => {
+             this.clicados++;
+             this.shadowRoot.querySelector(".clicados").innerText = `Você clicou ${this.clicados} vezes em itens`;
+            }
             ul.appendChild(li);
         }
         shadow.appendChild(ul);
