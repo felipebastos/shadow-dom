75addb3c8e80834f2f90927007a5c9293eba55fd
Felipe Bastos Nunes <felipebastos@ifce.edu.br>
2024-02-21 14:50:21 -0300

    Usando arrowfuncion para manter alguns dados de elementos

--- script.js
+++ script.js
@@ -1,28 +1,22 @@
 class Lista extends HTMLElement {
     constructor() {
         super();
-
-        this.clicados = 0;
-
     }
     connectedCallback() {
         const shadow = this.attachShadow({ mode: "open" });
 
         const lista = eval(this.getAttribute("texto"));
-        const h2 = document.createElement('h2');
-        h2.className = "clicados";
-        h2.innerText = `Você clicou ${this.clicados} vezes em itens`;
-
-        shadow.appendChild(h2);
 
         const ul = document.createElement('ul');
 
         for (let item of lista) {
+            let click = 0;
+
             const li = document.createElement('li')
             li.innerText = item;
             li.onclick = () => {
-             this.clicados++;
-             this.shadowRoot.querySelector(".clicados").innerText = `Você clicou ${this.clicados} vezes em itens`;
+             click++;
+             li.innerText = `${item} (Clicou ${click} vezes)`;
             }
             ul.appendChild(li);
         }
